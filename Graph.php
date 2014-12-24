b31dc7e9734eca806ded6ea9874aedd1e2681b62
Michael@Belgium <michaelvrld@gmail.com>
2014-12-24 02:32:15 +0100

    Updated with more stuff

--- Graph.php
+++ Graph.php
@@ -1,15 +1,24 @@
 <?php
-	include "SACNRMonitor.php"; // http://monitor.sacnr.com/api/php/SACNRMonitor.php
+    /* UPDATE 24/12/2014:
+     * Fixed the using_table part
+     * Added another table using more server info from SACNR monitor
+     * */
+
+	include "ucp/includes/SACNRMonitor.php";
 	
 	$serverid = 1638985; // SACNR Server ID | http://monitor.sacnr.com/server-<here is an id>.html
-	$hours = 44; // how much past hours should the graph show the playerbase. Max value = 44 !
-	$using_table = false; //Display a detailed table under it ? true or false
-
+	$hours = 24; // how much past hours should the graph show the playerbase. Max value = 44 !
+	$using_table = true; //Display a detailed table under it ? true or false
 
 	$monitor = new SACNRMonitor;
-	$query = $monitor->get_query_by_id($serverid);
-	$json = json_encode((array)$query);
-	
+
+	$query = (array)$monitor->get_query_by_id($serverid);
+    $info = (array)$monitor->get_info_by_id($serverid);
+
+	$json_query = json_encode($query);
+    $json_info = json_encode($info);
+
+
 	if($hours > 44 || $hours < 1) die("Invalid value: $hours");
 ?>
 
@@ -26,10 +35,10 @@
 		function drawChart()
 		{
 			var data = new google.visualization.DataTable();
-			data.addColumn('string', 'Date');
-			data.addColumn('number', 'Playercount');
+            data.addColumn('string', 'Date');
+            data.addColumn('number', 'Playercount');
 
-			var datavalues = <? echo $json ?>;
+			var datavalues = <? echo $json_query ?>;
 			var date = [];
 			for (i = datavalues.length-(<? echo $hours ?>* 4), j = datavalues.length; i < j; i++)
 			{
@@ -39,27 +48,52 @@
 				
 				data.addRow([date[1] + " " + date[2], parseInt(datavalues[i]["PlayersOnline"])]);
 			}
-			
-			if(<? echo $using_table ?> === 1)
+
+            var data_ = new google.visualization.DataTable();
+            datavalues = <? echo $json_info ?>;
+
+            data_.addColumn('string','Info');
+            data_.addColumn('string','Value');
+
+            data_.addRows([
+                ['Map', datavalues["Map"]],
+                ['Players', datavalues["Players"] + "/" + datavalues["MaxPlayers"]],
+                ['Average', datavalues["AvgPlayers"]],
+                ['Time', datavalues["Time"]],
+                ['Version', datavalues["Version"]],
+                ['Website', '<a href="http://'+ datavalues["WebURL"] +'">Click</a>'],
+                ['Password', (datavalues["Password"] === '0') ? 'No' : 'Yes'],
+                ['Host-tab', (datavalues["HostTab"] === '0') ? 'No' : 'Yes']
+            ]);
+
+            var enabled =  <? echo empty($using_table) ? 0 : 1 ?>;
+			if(enabled === 1)
 			{
 				var table = new google.visualization.Table(document.getElementById('table'));
 				table.draw(data, {showRowNumber: true, width: 600});
+                table = new google.visualization.Table(document.getElementById('info'));
+                table.draw(data_, {showRowNumber: false, width: 400, allowHtml: true});
 			}
-			
+
 			var options = {
-				title: "Players Online",
+				title:  datavalues["Hostname"] + " (" + datavalues["Gamemode"] + ")",
 				height: 400, 
 				legend: {position: 'bottom'},
 				orientation: 'horizontal'
 			};
-			
+
 			var chart = new google.visualization.LineChart(document.getElementById('chart'));
 			chart.draw(data, options);
 		}
-	</script>
+	    </script>
+        <style type="text/css">
+            #table, #info { display: inline-block;  vertical-align: top;}
+            #chart { margin-bottom: 10px; }
+        </style>
 	</head>
 	<body>
 		<div id="chart"></div>
 		<div id="table"></div>
+        <div id="info"></div>
 	</body>
 </html>
